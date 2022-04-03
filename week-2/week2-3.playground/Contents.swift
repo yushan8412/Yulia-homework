864734d48319bb6a54098603fc621b3b895c921a
yulia <ws61288412@gmail.com>
2022-04-04 00:55:28 +0800

    protocol

--- week-2/week2-3.playground/Contents.swift
+++ week-2/week2-3.playground/Contents.swift
@@ -1,19 +1,38 @@
 //1.
-struct Person {
-    var name : String
-}
+//struct Person {
+//    var name : String
+//}
 
 protocol PoliceMan {
     func arrestCriminals()
 }
 //2.
 //struct Person: PoliceMan {
+//    var name : String
+//    func arrestCriminals() {
+//    }
+//}
 
 //3.
 protocol ToolMan {
     func fixComputer()
 }
+
 //4.
+struct Person: PoliceMan {
+    var name : String
+    var toolMan : ToolMan
+    func arrestCriminals() {
+    }
+}
+//5
+struct Engineer : ToolMan {
+    func fixComputer() {
+    }
+}
+//6
+var steven = Person(name: "Steven", toolMan: Engineer())
+
 
 
 
