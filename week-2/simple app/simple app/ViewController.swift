891ac78a89903a744a007580f997c0e3983c072a
yulia <ws61288412@gmail.com>
2022-04-07 18:10:01 +0800

    week3

--- week-2/simple app/simple app/ViewController.swift
+++ week-2/simple app/simple app/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         
         view.backgroundColor = [UIColor.brown, UIColor.blue, UIColor.gray, UIColor.green, UIColor.orange, UIColor.red, UIColor.purple, ].randomElement()
         
-        sender.setTitleColor(view.backgroundColor, for: .normal)
+//        sender.setTitleColor(view.backgroundColor, for: .normal)
         middleLabel.text = [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus.", "Contrary to popular belief, Lorem Ipsum is not simply random text.", "Richard McClintock, a Latin professor at Hampden-Sydney College in ", "looked up one of the more obscure Latin words, consectetur",
         "from a Lorem Ipsum passage, and going through the cities of the word", "This book is a treatise on the theory of ethics, very popular during the.", "The first line of Lorem Ipsum, Lorem ipsum dolor sit amet..",].randomElement()
